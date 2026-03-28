8ae08d25253c49c8e40c6c420502b17433ed2162
Vitor Gomes Guimarães <vitorgguimaraes56@gmail.com>
2026-03-28 16:12:59 -0300

    yvwebsite

---
+++ script.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.15,
+});
+
+document.querySelectorAll('.reveal').forEach((el) => observer.observe(el));
